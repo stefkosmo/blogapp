dccbf0dde222b505f4e62c562e1b7b9943255688
Stefan Kosmirak <stefankosmirak@Stefans-iMac.home>
2015-02-17 11:44:08 +0000

    Biff opens casino

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,1 @@
-@extends('app')
-
-@section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in, great work there!
-					you have definately got the hang of this!
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+Biff strikes again
